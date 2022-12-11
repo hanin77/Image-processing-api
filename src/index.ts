bcd93caf4076b5c16305ba633de4265aec02dfc8
hanin77 <mohammadhanin77@gmail.com>
2022-12-11 22:05:49 +0100

    feat: add GET .../api/images endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import imagesRouter from './routes';
 const app = express();
+const port = 5000 || process.env.PORT;
 app.use('/api/images', imagesRouter);
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('server runs on port', 5000);
 });
+export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,50 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import sharp from 'sharp';
 import fs from 'fs';
-const imagesRouter = Router();
 import path from 'path';
-
-imagesRouter.get('/', (req, res) => {
-  const { filename, width, height } = req.query;
-  if (filename && width && height) {
-    const filePath = path.join(__dirname, `../../images/${filename}.jpg`);
-    //check if the requested img exist
-    if (!fs.existsSync(filePath)) {
-      return res.status(404).json({
-        status: 'failed',
-        message: 'the requested image to format was not found'
-      });
+const imagesRouter = Router();
+interface Query {
+  filename?: string;
+  width?: string;
+  height?: string;
+}
+imagesRouter.get(
+  '/',
+  async (req: Request, res: Response): Promise<void | Response> => {
+    const query: Query = req.query;
+    if (query.filename && query.width && query.height) {
+      const imgPath = path.join(
+        __dirname,
+        `../../images/${query.filename}.jpg`
+      );
+      //return 404 erro if the requested img not found
+      if (!fs.existsSync(imgPath)) {
+        return res.status(404).json({
+          status: 'fail',
+          message: 'the requested image to format was not found'
+        });
+      }
+      const formatedImgPath = path.join(
+        __dirname,
+        `../../images/thumb/${query.filename}-${query.width}x${query.height}.jpg`
+      );
+      //return the requested img if the file already formated with requested dim
+      if (fs.existsSync(formatedImgPath)) {
+        return res.sendFile(formatedImgPath);
+      }
+      //format the img with provided dim and send it to the client
+      await sharp(imgPath)
+        .resize(parseInt(query.width, 10), parseInt(query.height, 10))
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(formatedImgPath);
+      return res.sendFile(formatedImgPath);
     }
-    return res.sendFile(filePath);
+    return res.status(400).json({
+      status: 'fail',
+      message: 'not all required fields where provided'
+    });
   }
-  return res.status(400).json({
-    status: 'failed',
-    message: 'not all required fields where provided'
-  });
-});
+);
 
 export default imagesRouter;
