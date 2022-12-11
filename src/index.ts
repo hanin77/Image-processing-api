125c5d2af6820bfa7fa40f1e75890bacda8a3628
hanin77 <mohammadhanin77@gmail.com>
2022-12-11 19:50:21 +0100

    fix: typo in images route path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import imagesRouter from './routes';
 const app = express();
-app.use('api/images', imagesRouter);
+app.use('/api/images', imagesRouter);
 app.listen(5000, () => {
   console.log('server runs on port', 5000);
 });
