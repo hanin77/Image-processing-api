d91043a81fa2c0289171bd62c87987714e211f39
hanin77 <mohammadhanin77@gmail.com>
2022-12-12 15:33:16 +0100

    feat: display available images with names at home route

--- src/routes/viewRoutes.ts
+++ src/routes/viewRoutes.ts
@@ -2,5 +2,5 @@ import express from 'express';
 import viewController from '../controllers/viewController';
 
 const viewRoutes = express.Router();
-viewRoutes.get('/', viewController.getHome);
+viewRoutes.get('/home', viewController.getHome);
 export default viewRoutes;
